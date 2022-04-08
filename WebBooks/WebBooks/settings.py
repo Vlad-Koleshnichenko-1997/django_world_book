7bcf821db6cbf86a826a82ed1029a3652f7abe28
Vlad <krolik.zip@mail.ru>
2022-04-08 22:03:34 +0300

    Added files for deployment in heroku 20.08.2020

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
+import dj_database_url
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,12 +51,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'WebBooks.urls'
@@ -135,3 +139,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# статичние файлы (CSS, JavaScript, Images)
+# http://docs.djangoproject.com/en/1.10/howto/static-files/
+# Абсолютный путь к каталогу в котором collectstatic
+# будет собирать статистические файлы разметрывания
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
